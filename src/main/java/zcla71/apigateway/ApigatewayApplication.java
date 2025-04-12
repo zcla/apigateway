877d96c486f6ccae128e8cd2d5bcfa2eaebc31fe
zcla <zeclaudioaguiar@gmail.com>
2025-04-12 21:56:11 +0000

    Configuração feita

--- src/main/java/zcla71/apigateway/ApigatewayApplication.java
+++ src/main/java/zcla71/apigateway/ApigatewayApplication.java
@@ -2,12 +2,12 @@ package zcla71.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApigatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
-
 }
